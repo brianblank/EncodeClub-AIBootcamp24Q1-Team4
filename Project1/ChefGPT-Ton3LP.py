5aa6619a14f64711d91472641af39a1b7aed1e67
Cheuk Chan <8016068+ckxddd0324@users.noreply.github.com>
2024-03-24 18:40:33 -0400

    fix Ton3LP to run

--- Project1/ChefGPT-Ton3LP.py
+++ Project1/ChefGPT-Ton3LP.py
@@ -1,16 +1,14 @@
 import os
+import sys
 from openai import OpenAI
-client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY"),
-)
-model = "gpt-3.5-turbo"
+client = OpenAI()
 
 # To make things interesting, you could make each member of your group a different "personality" for the AI chef
 # For example, define a "focused and detailist vegan French chef" for one script and a "fun and energetic Mexican chef that loves spicy food" for another
 messages = [
      {
           "role": "system",
-          "content": "You are the best sushi chef in the world. You are able to provide all the ingredients for the dish and able to provide a super-precise steps and guide for the dish. You are a perfectionist."
+          "content": "You are the best Japanese chef in the world. You are able to provide dish name can cook with given ingredients or suggest for the dish and able to provide a super-precise steps and guide for the dish. You are a perfectionist."
      }
 ]
 
@@ -21,26 +19,19 @@ messages.append(
      }
 )
 
-# If the user passes a different prompt than these three scenarios as the first message, the AI should deny the request and ask to try again
-messages.append(
-     {
-          "role": "system",
-          "content":  "If the user passes something different than the three scenarios as th first thing, you will not answer them and request the user to ask again",
-     }
-)
 
 # If the user passes one or more ingredients, the AI should suggest a dish name that can be made with these ingredients
 messages.append(
      {
           "role": "system",
-          "content":  "If the user passes one or more ingredient, you will suggest the best healthiest dish that can be made with these items",
+          "content":  "If the user gives you one or more ingredients, you will try to only suggest dish name",
      }
 )
 # Try to make the AI suggest the dish name only, and not the recipe at this stage
 messages.append(
      {
           "role": "system",
-          "content":  "You will only give the dish name only, not going to give out receipt",
+          "content":  "You will suggest a dish name with the given ingredients",
      }
 )
 # If the user passes a dish name, the AI should give a recipe for that dish
@@ -59,29 +50,30 @@ messages.append(
 )
 
 
-while True:
-    print("\n")
-    user_input = input()
-    messages.append(
-        {
-            "role": "user",
-            "content": user_input
-        }
-    )
-    stream = client.chat.completions.create(
+# User input
+if len(sys.argv) > 1:
+    user_input = sys.argv[1]
+else:
+    user_input = input("Type the name of the dish, a set of ingredients, or a recipe for a dish:\n")
+messages.append(
+    {
+        "role": "user",
+        "content": f"{user_input}"
+    }
+)
+
+model = "gpt-3.5-turbo"
+
+stream = client.chat.completions.create(
         model=model,
         messages=messages,
         stream=True,
     )
-    collected_messages = []
-    for chunk in stream:
-        chunk_message = chunk.choices[0].delta.content or ""
-        print(chunk_message, end="")
-        collected_messages.append(chunk_message)
-    
-    messages.append(
-        {
-            "role": "system",
-            "content": "".join(collected_messages)
-        }
-    )
+
+collected_messages = []
+for chunk in stream:
+    chunk_message = chunk.choices[0].delta.content or ""
+    print(chunk_message, end="")
+    collected_messages.append(chunk_message)
+print("\n")
+
